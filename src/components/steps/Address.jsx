9408f693e01c9b59faf2a047d6392073ead86809
AldairMC <aldairmc2023@gmail.com>
2022-05-15 12:07:11 -0500

    :sparkles: Add component Address

--- src/components/steps/Address.jsx
+++ src/components/steps/Address.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import AppContext from '../../context/AppContext'
 
-const Address = ({code}) => {
+const Address = ({ info, changeInfo }) => {
+  const { address } = useContext(AppContext)
+
   return (
-    <div>{code} Address</div>
+    <div className='Address'>
+      <h1 className='H1_Address_Title'>{info.code}</h1>
+      <p className='P_Address_Description'>{info.descripcion}</p>
+      <input
+        type="text"
+        className='Input_Address_Value'
+        value={address}
+        name={info.nameState}
+        placeholder={info.name}
+        onChange={changeInfo}
+      />
+    </div>
   )
 }
 

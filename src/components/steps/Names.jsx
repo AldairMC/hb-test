f97e7fd5eb2bc99bf8a55ab733bd73f32218dc12
AldairMC <aldairmc2023@gmail.com>
2022-05-15 07:30:25 -0500

    :sparkles: Component Names ready

--- src/components/steps/Names.jsx
+++ src/components/steps/Names.jsx
@@ -10,11 +10,12 @@ const Names = ({ info }) => {
   }
 
   return (
-    <div>
-      <h1>{info.code}</h1>
-      <p>{info.descripcion}</p>
+    <div className='Names'>
+      <h1 className='H1_Names_Title'>{info.code}</h1>
+      <p className='P_Names_Description'>{info.descripcion}</p>
       <input 
         type="text"
+        className='Input_Names_Value'
         value={names}
         name={info.nameState}
         placeholder={info.name}

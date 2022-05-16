2ac7fd7016f0ca680668a8b2cc0e46542d305c53
AldairMC <aldairmc2023@gmail.com>
2022-05-15 20:28:51 -0500

    :sparkles: Add component Floor

--- src/components/steps/Floors.jsx
+++ src/components/steps/Floors.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import AppContext from '../../context/AppContext'
 
-const Floors = () => {
+const Floor = ({ info, changeInfo }) => {
+  const { floor } = useContext(AppContext)
+
   return (
-    <div>Floors</div>
+    <div className='Floor'>
+      <h1 className='H1_Floor_Title'>{info.code}</h1>
+      <p className='P_Floor_Description'>{info.descripcion}</p>
+      <input 
+        type="number"
+        max={5}
+        min={1}
+        className='Input_Floor_Value'
+        value={floor}
+        name={info.nameState}
+        placeholder={info.name}
+        onChange={changeInfo}
+      />
+    </div>
   )
 }
 
-export default Floors;
+export default Floor;
--- src/utils/helpes.js
+++ src/utils/helpes.js
@@ -1,1 +1,10 @@
-// Code here
+export const validNumber = (value) => {
+    const convertNumber = Number(value)
+    if(convertNumber < 0){
+        return 1
+    } else if(convertNumber > 50){
+        return 50
+    } else {
+        return convertNumber
+    }
+}

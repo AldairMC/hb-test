6e3229fffa1145b319f2cdd68d7f988702f0b82b
AldairMC <aldairmc2023@gmail.com>
2022-05-16 13:25:38 -0500

    :art: Add little changes :art: Change title project :lipstick: Change color icon :construction: Avtive stepper

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -1,13 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import AppContext from '../context/AppContext'
 import { Link } from 'react-router-dom'
 
 import { Paths } from '../constants/paths'
 
 const Stepper = () => {
+    const initialState = {
+        "/nombre-completo": false,
+        "/email": false,
+        "/direccion-apartamento": false,
+        "/numero-pisos": false,
+        "/beneficios": false
+    }
+
+    const [ pathActived, setPathAtived ] = useState(initialState)
     const { data } = useContext(AppContext)
     const Links = Paths.filter(info => info.visible)
 
+    const handlePatActive = (path) => {
+        const pathname = window.location.pathname
+        const uriPath = !pathActived[path] || pathname === path
+        setPathAtived({
+            [path]: uriPath
+        })
+    }
+    
     return (
         <div className='Stepper'>
             {
@@ -17,7 +34,7 @@ const Stepper = () => {
                             <Link to={info.path} className={`${!(data[info.nameState])?"__empty_title" : "__full_title"}`}>
                                 {
                                     !(data[info.nameState])? <i className={`${info.icon} __empty_stepper`}></i> : <i className="fa-solid fa-circle-check __full_stepper"></i>
-                                } {info.name}
+                                } <span onClick={() => handlePatActive(info.path)} className={`${pathActived[info.path]? "Span_Stepper_Name" : ""}`}>{info.name}</span>
                             </Link>
                         }
                     </div>
